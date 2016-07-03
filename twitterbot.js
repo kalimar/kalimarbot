f088d7978d6198ef616a9277dc68a9041580b4c6
Kalimar Maia <kmaia@customink.com>
2016-07-02 21:56:15 -0400

    Basic app setup

--- twitterbot.js
+++ twitterbot.js
@@ -1,1 +1,26 @@
 require('dotenv').config();
+var restclient = require('node-restclient');
+var Twit = require('twit');
+var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
+
+var app = express();
+app.get('/', function(req, res){
+  res.send('kalimaaaaaaaa');
+});
+
+// app.listen(process.env.PORT || 3000, function(){
+// })
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+var db = mongoose.connection;
+var kalimarSchema = new mongoose.Schema({
+  title: String,
+  value: Number
+});
+var KalimarDb = mongoose.model('KalimarDb, kalimarSchema');
+mongoose.connect('mongodb://localhost/twitterbot');
+mongoose.connect('mongodb:');
